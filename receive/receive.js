1df8ce5a7f5b821673f2d1a3639d221be95c6ad2
ayushkayshab04 <ayushkayshab04@gmail.com>
2022-03-25 15:53:54 +0530

    Recent one

--- receive/receive.js
+++ receive/receive.js
@@ -2,26 +2,13 @@ const express = require("express")
 const {connect} = require ("./controller/signup.js")
 const user = require("./route/user.js")
 
-connect();
-
 const app = express();
 
-
-
-
+connect();
 
 app.use(express.json())
 
-
 app.use("/user",user)
-
-
-
-
-
-
-
-
 
 app.listen(3000, ()=>{
     console.log("erver started on port 3000")
--- send/sendcreds.js
+++ send/sendcreds.js
@@ -2,17 +2,13 @@ const express = require("express")
 const send = require("./routes/creds.js")
 const {connect} = require("./controller/send.js")
 
-
-connect();
 const app = express();
 
+connect();
 
 app.use(express.json())
 
 app.use("/send",send)
-
-
-
 
 app.listen(5000,()=>{
     console.log("server started on port 5000")
